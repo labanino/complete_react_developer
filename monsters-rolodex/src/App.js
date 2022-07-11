205d19ba4b12d5061ba04a93edb1ddc2999fd698
Alex Labanino <ghostrider6610@gmail.com>
2022-07-10 21:37:40 -0400

    Keys for Mapping

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,29 +9,35 @@ class App extends Component {
     this.state = {
       monsters: [
         {
-          name: 'Linga'
+          name: 'Linga',
+          id: '473894sd',
         },
         {
-          name: 'Frank'
+          name: 'Frank',
+          id: '851123er',
         },
         {
-          name: 'Jacky'
+          name: 'Jacky',
+          id: '748hgja1',
         },
         {
-          name: 'Andrei'
-        }
-      ]
+          name: 'Andrei', 
+          id: 'kdksjk757',
+        },
+      ],
     };
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>;
-          })
-        }
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
