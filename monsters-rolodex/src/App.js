ac8931c7fad7b58f6979b9b9e8e294ee68d66189
Alex Labanino <ghostrider6610@gmail.com>
2023-02-14 20:25:22 -0500

    43. Storing Original Data

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,42 +3,62 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      monsters: [],
-    };
-  }
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+    }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => 
-        this.setState(
-          () => {
-            return {monsters: users};
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) =>
+                this.setState(
+                    () => {
+                        return { monsters: users };
+                    },
+                    () => {
+                        console.log(this.state);
+                    }
+                )
+            );
+    }
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
+    render() {
+        console.log('render');
+
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        });
+
+        return (
+            <div className="App">
+                <input
+                    className='search-box'
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={(event) => {
+                        const searchField = event.target.value.toLocaleLowerCase();
+                        this.setState(
+                            () => {
+                                return { searchField };
+                            }
+                        );
+                    }}
+                />
+                {filteredMonsters.map((monster) => {
+                    return (
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
